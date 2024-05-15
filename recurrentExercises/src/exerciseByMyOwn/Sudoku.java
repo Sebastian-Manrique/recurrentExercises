d00c037044d3c8f4a8a20b037d4d7c051621c95d
SebastianManrique <sebastianmanriquemontiel@gmail.com>
2024-05-15 20:08:30 +0200

    add sql with java swing

--- recurrentExercises/src/exerciseByMyOwn/Sudoku.java
+++ recurrentExercises/src/exerciseByMyOwn/Sudoku.java
@@ -20,20 +20,20 @@ public class Sudoku implements ActionListener{
 		f.setSize(400,400);
 		Font newTextFieldFont=new Font("Default", Font.PLAIN, 15);//60 //Create font. Code from https://java2everyone.blogspot.com/2008/12/set-jtextfield-text-size.html
 		JPanel panel = new JPanel();
-	    panel.setLayout(new GridLayout(4, 6, 2, 2)); // 4 rows, 6 columns, with 2 pixels space
-		//Create a window using JFrame with no title (  )
+	    panel.setLayout(new GridLayout(4, 4, 2, 2)); // 4 rows, 6 columns, with 2 pixels space
+		
 		for (int i=0; i<6;i++) { //Row
 			for (int j = 0; j < 4; j++) { //Column	
-				JTextField t=new JTextField(" i "+i+" j "+j);
+				JTextField t=new JTextField("i "+i+" j "+j); //Create a window using JFrame with nothing (  )
 				t.setFont(newTextFieldFont); //Set JTextField font using new created font
 				t.setBounds(50*i,60*j, 50,60); //Left, Top, width text, height text
-				if (i==2)   {	//To make the borders biggers for the second row
+				if (i==2) {	//To make the borders biggers for the second row
 					  Border oldBorder = t.getBorder();
 					  Border blackBorder = BorderFactory.createMatteBorder(0, 0, 0, 2, Color.BLACK); //From https://stackoverflow.com/questions/11935188/is-there-any-way-to-get-one-sidei-e-right-bordered-line-of-the-jtextfield-co
 					  Border newBorder = BorderFactory.createCompoundBorder(blackBorder, oldBorder);
 					  t.setBorder(newBorder);
 				}
-				if (j==2)   {	//To make the borders biggers for the second column
+				if (j==2) {	//To make the borders biggers for the second column
 					  Border oldBorder = t.getBorder();
 					  Border blackBorder = BorderFactory.createMatteBorder(2, 0, 0, 0, Color.BLACK);
 					  Border newBorder = BorderFactory.createCompoundBorder(blackBorder, oldBorder);
@@ -51,7 +51,8 @@ public class Sudoku implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) { //The buttom do this
 		if (e.getSource()==button) {
-			   System.exit(0);
+//			JOptionPane.showInternalMessageDialog(null, "fin");		
+			System.exit(0);
 		}
 	}
 	public static void main(String[] args) {
