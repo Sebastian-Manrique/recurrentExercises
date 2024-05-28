3558d37dc7c195683b4d4bd8313f0f6706d85aa4
danielluru <danielluru77@gmail.com>
2024-05-28 21:08:30 +0200

    intento de arreglo

--- src/clases/PanelRepartidor.java
+++ src/clases/PanelRepartidor.java
@@ -23,34 +23,33 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class PanelRepartidor extends JFrame{
+public class PanelRepartidor extends JFrame {
 	String url = "jdbc:mysql://localhost:3307/proyectojava";
 	private JTextField textField;
 	private JTable table;
 	DefaultTableModel tabla;
-	String [] titulos = new String[] {
-			"Id Envio", "Id Paquete", "Id Trabajador"
-	};
-	String [] datos = new String[3];
-	
+	String[] titulos = new String[] { "Id Envio", "Id Paquete", "Id Trabajador" };
+	String[] datos = new String[3];
+
 	public PanelRepartidor() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(PanelRepartidor.class.getResource("/imagenes/imagen.png")));
 		getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Id del envio");
 		lblNewLabel.setBounds(20, 23, 87, 30);
 		getContentPane().add(lblNewLabel);
-		
+
 		textField = new JTextField();
 		textField.setToolTipText("");
 		textField.setBounds(97, 21, 111, 35);
 		getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		table = new JTable();
 		table.setBounds(20, 85, 807, 321);
+		actualizarTabla(tabla);
 		getContentPane().add(table);
-		
+
 		JButton btnNewButton = new JButton("Eliminar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -63,37 +62,29 @@ public class PanelRepartidor extends JFrame{
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Que se acabe al cerrar
 	}
-	
+
 	public void actualizarTabla(DefaultTableModel tabla) {
-        String sSQL = "SELECT * FROM ENVIOS";
+        String sSQL = "SELECT * FROM ENVIO";
         tabla = new DefaultTableModel(null, titulos);
-        Connection c = null;
+        Connection c;
+        table.setDefaultRenderer(Object.class, new CustomTableCellRenderer());
 		try {
+			System.out.println("hlaa");
 			c = DriverManager.getConnection(url, "root", "1234");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Statement st = c.createStatement();
+	        ResultSet rs = st.executeQuery(sSQL);
+	        while(rs.next()) {
+	                datos[0]=rs.getInt("Id Envio")+"";
+	                datos[1]=rs.getInt("Id Paquete")+"";
+	                datos[2]=rs.getInt("Id Trabajador")+"";
+	                tabla.addRow(datos);
+	        } 
+	     }catch (SQLException e) {
+	            JOptionPane.showMessageDialog(null, "error " + e, "ERROR", JOptionPane.ERROR_MESSAGE);
+				e.printStackTrace();
 		}
 //        tabla.addRow(titulos);
-        table.setDefaultRenderer(Object.class, new CustomTableCellRenderer());
-        try {
-            Statement st = c.createStatement();
-            ResultSet rs = st.executeQuery(sSQL);
-            while(rs.next()) {
-                if(rs.getString("Comprado-Alquilado").equalsIgnoreCase("Si")) { // Si esta comprado alquilado no se añade
-                    continue;
-                }
-                datos[0]=rs.getInt("Id Envio")+"";
-                datos[1]=rs.getInt("Id Paquete")+"";
-                datos[2]=rs.getInt("Id Trabajador")+"";
-                tabla.addRow(datos);
-            }
-
-            table.setModel(tabla);
-        } catch (SQLException e) {
-            // TODO: handle exception
-            JOptionPane.showMessageDialog(null, "error " + e, "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
+      
  }
 	   static class CustomTableCellRenderer extends DefaultTableCellRenderer {
 	         
