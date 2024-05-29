6b5f5921462ce85b0b5e1f11620b80e594730143
WildRepublicMini <smanriquemontiel@gmail.com>
2024-05-30 01:39:46 +0200

    knowing wich side of the sudoku with ifs

--- src/exerciseByMyOwn/Sudoku.java
+++ src/exerciseByMyOwn/Sudoku.java
@@ -3,9 +3,15 @@ package exerciseByMyOwn;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GraphicsConfiguration;
 import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.plaf.synth.Region;
@@ -15,17 +21,48 @@ public class Sudoku implements ActionListener{
 	JButton button = new JButton("End");
 	
 	public Sudoku() {
+//		Random rand = new Random();
+//		int random = rand.nextInt(1,3);
+//		System.out.println(random);
+		GraphicsConfiguration config = f.getGraphicsConfiguration();
+	    Rectangle bounds = config.getBounds();
+	    Insets insets = Toolkit.getDefaultToolkit().getScreenInsets(config);
+		int x = bounds.x + bounds.width - insets.right - f.getWidth();
+	    int y = bounds.y + insets.top;
+	    f.setLocation(x-1000, y+170);
 		ImageIcon img = new ImageIcon("images&Fonts/sudokuIcon.png");
 		f.setIconImage(img.getImage());
 		f.setTitle("Sudoku");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//Close on exit
-		f.setSize(600,650);
-		Font newTextFieldFont=new Font("Roboto", Font.PLAIN, 30);//60 //Create font. Code from https://java2everyone.blogspot.com/2008/12/set-jtextfield-text-size.html
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close on exit
+		f.setSize(600, 650);
+		Font newTextFieldFont = new Font("Roboto", Font.PLAIN, 30);// Create font. Code from https://java2everyone.blogspot.com/2008/12/set-jtextfield-text-size.html
 		JPanel panel = new JPanel(new GridLayout(6, 6));
-		for (int i=0; i<6;i++) { //Row
-			for (int j = 0; j <6; j++) { //Column	
-				JTextField t = new JTextField(" i " + i + ", j " + j);
-				t.setFont(newTextFieldFont); //Set JTextField font using new created font
+		for (int i = 0; i < 6; i++) { // Row
+			for (int j = 0; j < 6; j++) { // Column
+				JTextField t = new JTextField("i " + i + ", j " + j);
+				t.setFont(newTextFieldFont); // Set JTextField font using new created font
+				if (j < 3) {
+					t = new JTextField("i " + i + ", j " + j);
+					t.setEditable(false);
+					Border oldBorder = t.getBorder();
+					Border blackBorder = BorderFactory.createMatteBorder(0, 0, 2, 0, Color.PINK); // From https://stackoverflow.com/questions/11935188/is-there-any-way-to-get-one-sidei-e-right-bordered-line-of-the-jtextfield-co
+					Border newBorder = BorderFactory.createCompoundBorder(blackBorder, oldBorder);
+					t.setBorder(newBorder);
+				} else if (j >= 3) {
+					t = new JTextField("i " + i + ", j " + j);
+					t.setEditable(false);
+					Border oldBorder = t.getBorder();
+					Border blackBorder = BorderFactory.createMatteBorder(0, 0, 2, 0, Color.GREEN); // From https://stackoverflow.com/questions/11935188/is-there-any-way-to-get-one-sidei-e-right-bordered-line-of-the-jtextfield-co
+					Border newBorder = BorderFactory.createCompoundBorder(blackBorder, oldBorder);
+					t.setBorder(newBorder);
+				}
+//				if (j<5) {
+//					t = new JTextField("2");
+//					t.setEditable(false);
+//				}
+//				else {
+//					t = new JTextField("i "+i+", j "+j);
+//				}
 				panel.add(t);
 				if (j==2)   {	//To make the borders biggers for the second row
 					  Border oldBorder = t.getBorder();
@@ -45,13 +82,21 @@ public class Sudoku implements ActionListener{
 	    button.setFocusable(false);
 	    button.addActionListener(this);
 	    f.add(button, BorderLayout.SOUTH); // Añadir el botón al fondo del contenedor principal
-        f.setVisible(true);
+        
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) { //The buttom do this
 		f.dispose();
 	}
 	public static void main(String[] args) {
-		Sudoku sudoku = new Sudoku();
+		Sudoku susuSudoku = new Sudoku();
+		susuSudoku.f.setVisible(true);
+//		Random rand = new Random();
+//		int rand_dub = rand.nextInt(1,7);
+//		for (int countForFillers = 0; countForFillers < rand_dub; countForFillers++) {
+//			int rand_cells = rand.nextInt(1,7);
+//			JLabel nonEditJLabel = new JLabel("jl: " + rand_cells);
+//			System.out.println(rand_cells);
+//			panel.add(nonEditJLabel);
 	}
 }  
