efb058a54f6be73ce25f3df904a5f89f2d0072ca
WildRepublicMini <smanriquemontiel@gmail.com>
2024-06-02 17:11:33 +0200

    adding the columns comprobation

--- src/exerciseByMyOwn/Sudoku.java
+++ src/exerciseByMyOwn/Sudoku.java
@@ -27,37 +27,54 @@ public class Sudoku extends javax.swing.JFrame {
 		Font roboto = new Font("Roboto", Font.PLAIN, 30);// Create font. Code from https://java2everyone.blogspot.com/2008/12/set-jtextfield-text-size.html
 		JPanel panel = new JPanel(new GridLayout(6, 6));
 		JTextField t = new JTextField();
-		ArrayList<Integer> intDestination = new ArrayList<Integer>();
+		ArrayList<ArrayList<Integer>> intAlreadyIn = new ArrayList<ArrayList<Integer>>();
 		for (int i = 0; i < 6; i++) { // Row
+			ArrayList<Integer> intDestination = new ArrayList<Integer>();
 			for (int j = 0; j < 6; j++) { // Column
-				t = new JTextField("posi i "+i);
+				t = new JTextField();
 				t.setFont(roboto); // Set JTextField font using new created font
 				if (j < 3) {
-					int random = rand.nextInt(1, 7);
-					boolean repe = isRandomReped(intDestination, random);
-					if (!repe) { // If repe is false
-						intDestination.add(random);
-						t = new JTextField("" + random);
-						t.setFont(roboto);
-					}
+					boolean repeRows = false;
+					boolean repeColumns = false;
+					do {
+						int random = rand.nextInt(1, 7);
+						repeRows = isRandomRepedRows(intDestination, random);
+						repeColumns = isRandomRepedColumns(intAlreadyIn, random, j);
+						if (!repeRows && !repeColumns) { // If repe is false
+							intDestination.add(random);
+							t = new JTextField("" + random);
+							t.setFont(roboto);
+						}
+					} while (repeRows || repeColumns);
 					t.setEditable(false);
 					Border oldBorder = t.getBorder();
 					Border pinkBorder = BorderFactory.createMatteBorder(0, 0, 2, 0, Color.PINK); // From https://stackoverflow.com/questions/11935188/is-there-any-way-to-get-one-sidei-e-right-bordered-line-of-the-jtextfield-co
 					Border newBorder = BorderFactory.createCompoundBorder(pinkBorder, oldBorder);
 					t.setBorder(newBorder);
-					columnOfTheSudoku(i,t);
+					columnOfTheSudoku(i, t);
 				} else if (j >= 3) {
-					t = new JTextField("i " + i + ", j " + j);
-					t.setFont(roboto);
-					t.setEditable(false);
 					Border oldBorder = t.getBorder();
 					Border greenBorder = BorderFactory.createMatteBorder(0, 0, 2, 0, Color.GREEN);
 					Border newBorder = BorderFactory.createCompoundBorder(greenBorder, oldBorder);
+//					t.setBorder(newBorder);
+					boolean repe = false;
+					do {
+						int random = rand.nextInt(1, 7);
+						repe = isRandomRepedRows(intDestination, random);
+						if (!repe) { // If repe is false
+							intDestination.add(random);
+							t = new JTextField("" + random);
+							t.setFont(roboto);
+						}
+					} while (repe);
+					t.setEditable(false);
 					t.setBorder(newBorder);
+					columnOfTheSudoku(i, t);
 				}
 				panel.add(t);
 				linesMethod(i, j, t);
 			}
+			intAlreadyIn.add(intDestination);
 		}
 		f.add(panel, BorderLayout.CENTER); // Adding the panel at the center of the component
 		button.setFocusable(false);
@@ -67,7 +84,7 @@ public class Sudoku extends javax.swing.JFrame {
 	}
 	
 	private void columnOfTheSudoku(int i, JTextField t) {
-		if (i%2==0) {
+		if (i==0 || i==1) {
 			Border oldBorder = t.getBorder();
 			Border pinkBorder = BorderFactory.createMatteBorder(0, 2, 0, 0, Color.BLUE);
 			Border newBorder = BorderFactory.createCompoundBorder(pinkBorder, oldBorder);
@@ -81,7 +98,7 @@ public class Sudoku extends javax.swing.JFrame {
 		Insets insets = Toolkit.getDefaultToolkit().getScreenInsets(config);
 		int x = bounds.x + bounds.width - insets.right - f.getWidth();
 		int y =  bounds.y + bounds.height - insets.top - f.getHeight(); // I just do that copying the int x and changing the values
-		f.setLocation(x/3, y/3);
+		f.setLocation(x/3, y/3-100);
 		ImageIcon img = new ImageIcon("images&Fonts/sudokuIcon.png");
 		f.setIconImage(img.getImage());
 		f.setTitle("Sudoku");
@@ -89,7 +106,7 @@ public class Sudoku extends javax.swing.JFrame {
 		f.setSize(600, 650);
 	}
 	
-	private boolean isRandomReped(ArrayList<Integer> intDestination, int random) { // This method has the random and the array to prove its not repeated
+	private boolean isRandomRepedRows(ArrayList<Integer> intDestination, int random) { // This method has the random and the array to prove its not repeated
 		for (int k = 0; k < intDestination.size(); k++) {
 			if (intDestination.get(k) == random) {
 				System.out.println("Repe " + random);
@@ -99,6 +116,15 @@ public class Sudoku extends javax.swing.JFrame {
 		return false;
 	}
 
+	private boolean isRandomRepedColumns(ArrayList<ArrayList<Integer>> intAlreadyIn, int random, int j) { // This method has the random and the array to prove its not repeated
+		for (int k = 0; k < intAlreadyIn.size(); k++) {
+			if (intAlreadyIn.get(k).get(j) == random) {
+				System.out.println("Repe columns " + random);
+				return true;
+			}
+		}
+		return false;
+	}
 	private void linesMethod(int i, int j, JTextField t) {
 		if (j == 2) { // To make the borders biggers for the second row
 			Border oldBorder = t.getBorder();
