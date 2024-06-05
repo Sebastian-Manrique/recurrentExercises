1620113373df1458fe43bb79d7fad797586477f1
WildRepublicMini <smanriquemontiel@gmail.com>
2024-06-06 01:43:02 +0200

    Now the file writer works

--- src/clases/CrearEnvio.java
+++ src/clases/CrearEnvio.java
@@ -31,7 +31,7 @@ import java.awt.event.ActionEvent;
 public class CrearEnvio extends JFrame {
 
 	static String url = "jdbc:mysql://localhost:3306/proyectojava";
-	
+
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
@@ -41,21 +41,14 @@ public class CrearEnvio extends JFrame {
 	private JTable table_1;
 	private JTable table_2;
 
-	String[] titulos = new String[] {
-			"ID","Nombre","Apellido","Cargo","Password"
-			};
+	String[] titulos = new String[] { "ID", "Nombre", "Apellido", "Cargo", "Password" };
 	String[] datos = new String[5];
-	
-	String[] titulos1 = new String[] {
-			"ID","Nombre Paquete","Peso"
-			};
+
+	String[] titulos1 = new String[] { "ID", "Nombre Paquete", "Peso" };
 	String[] datos1 = new String[3];
 
-	String[] titulos2 = new String[] {
-			"ID Envio","ID Paquete","ID Trabajador"
-			};
+	String[] titulos2 = new String[] { "ID Envio", "ID Paquete", "ID Trabajador" };
 	String[] datos2 = new String[3];
-	
 
 	public CrearEnvio() {
 		setVisible(true);
@@ -67,62 +60,59 @@ public class CrearEnvio extends JFrame {
 		setLocationRelativeTo(null);
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("ID Envio");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel.setBounds(10, 11, 70, 29);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("ID Paquete");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_1.setBounds(230, 11, 96, 29);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("ID Trabajador");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_2.setBounds(494, 11, 112, 29);
 		contentPane.add(lblNewLabel_2);
-		
-		
-		
+
 		textField = new JTextField();
 		textField.setBounds(82, 8, 138, 32);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(336, 11, 137, 32);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		textField_2 = new JTextField();
 		textField_2.setText("");
 		textField_2.setBounds(616, 9, 151, 32);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
-		
-		
+
 		DefaultTableModel tabla = new DefaultTableModel(titulos, 0);
 		table = new JTable(tabla);
-		
+
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setBounds(31, 89, 274, 263);
 		contentPane.add(scrollPane);
-		
+
 		DefaultTableModel tabla1 = new DefaultTableModel(titulos1, 0);
 		table_1 = new JTable(tabla1);
-		
+
 		JScrollPane scrollPane_1 = new JScrollPane(table_1);
 		scrollPane_1.setBounds(366, 89, 274, 263);
 		contentPane.add(scrollPane_1);
-		
+
 		DefaultTableModel tabla2 = new DefaultTableModel(titulos2, 0);
 		table_2 = new JTable(tabla2);
-		
+
 		JScrollPane scrollPane_2 = new JScrollPane(table_2);
 		scrollPane_2.setBounds(680, 89, 274, 263);
 		contentPane.add(scrollPane_2);
-		
+
 		JButton btnNewButton = new JButton("Crear");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -131,16 +121,16 @@ public class CrearEnvio extends JFrame {
 				String idEmp = textField_2.getText();
 				Envio envio = new Envio(idEnvio, idPaq, idEmp);
 				sql.crearEnvio(envio);
-				actualizarTablaEnvios(tabla2,0);
+				actualizarTablaEnvios(tabla2, 0);
 			}
 		});
 		btnNewButton.setBounds(810, 9, 147, 37);
 		contentPane.add(btnNewButton);
-		
 		JButton guardarEnFicheroBtn = new JButton("Guardar en un fichero");
 		guardarEnFicheroBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int ans = Integer.parseInt(JOptionPane.showInputDialog(null, "Que quieres\n1.Empleados\n2.Paquetes\n3.Envios"));
+				int ans = Integer
+						.parseInt(JOptionPane.showInputDialog(null, "Que quieres\n1.Empleados\n2.Paquetes\n3.Envios"));
 				switch (ans) {
 				case 1: {
 					actualizarTablaEmpleados(tabla, 1);
@@ -162,17 +152,19 @@ public class CrearEnvio extends JFrame {
 		guardarEnFicheroBtn.setBounds(405, 382, 159, 55);
 		contentPane.add(guardarEnFicheroBtn);
 		setLocationRelativeTo(null);
-		
+
 		actualizarTablaEmpleados(tabla, 0);
 		actualizarTablaPaquetes(tabla1, 0);
 		actualizarTablaEnvios(tabla2, 0);
-		
-	}
-	public void actualizarTablaEmpleados(DefaultTableModel tabla, int imprime) {
-        
-		tabla = new DefaultTableModel(null, titulos);
 
+	}
+
+	public void actualizarTablaEmpleados(DefaultTableModel tabla, int imprime) {
+
+		tabla = new DefaultTableModel(null, titulos);
+		
 		try {
+			ArrayList<String> datosFileArrayList = new ArrayList<String>();
 			Connection c = DriverManager.getConnection(url, "root", "1234");
 			Statement st = c.createStatement();
 			String sSQL = "SELECT * FROM TRABAJADORES";
@@ -183,119 +175,108 @@ public class CrearEnvio extends JFrame {
 				datos[2] = rs.getString("apellido");
 				datos[3] = rs.getString("cargo");
 				datos[4] = rs.getString("password");
-				if (imprime==1) {
-					String data = rs.getString("idTrabajadores") + " " + rs.getString("nombre") + " "
-							+ rs.getString("apellido") + " " + rs.getString("cargo") + " " + rs.getString("password");
-					ArrayList<String> datosFileArrayList = new ArrayList<String>();
-					datosFileArrayList.add(data);
-					mtdFileWriter(datosFileArrayList, "trabajadores");
-				}
+				String data = rs.getString("idTrabajadores") + " " + rs.getString("nombre") + " "
+						+ rs.getString("apellido") + " " + rs.getString("cargo") + " " + rs.getString("password");
+				datosFileArrayList.add(data);
 				tabla.addRow(datos);
+			}
+			if (imprime == 1) {
+				mtdFileWriter(datosFileArrayList, "trabajadores");
 			}
 			table.setModel(tabla);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-}	
+	}
 
-public void actualizarTablaPaquetes(DefaultTableModel tabla1,  int imprime) {
-	
+	public void actualizarTablaPaquetes(DefaultTableModel tabla1, int imprime) {
+
 		tabla1 = new DefaultTableModel(null, titulos1);
 
-      try {
-      	Connection c = DriverManager.getConnection(url, "root", "1234");
-      	Statement st = c.createStatement();
-          String sSQL = "SELECT * FROM Paquete";
-          ResultSet rs = st.executeQuery(sSQL);
-          
-          while(rs.next()) {
-              datos1[0]=rs.getString("idPaquete");
-              datos1[1]=rs.getString("nombrePaquete");
-              datos1[2]=rs.getString("pesoPaquete");
-              if (imprime==2) {
-            	  String data = rs.getString("idPaquete") + " " + rs.getString("nombrePaquete") + " "
-  						+ rs.getString("pesoPaquete");
-  				ArrayList<String> datosFileArrayList = new ArrayList<String>();
-  				datosFileArrayList.add(data);
-  				mtdFileWriter(datosFileArrayList, "paquetes");
-              }
-              for(String dato:datos1) {
-              	System.out.println(dato);
-              	
-              }
-              tabla1.addRow(datos1);
-          }
+		try {
+			ArrayList<String> datosFileArrayList = new ArrayList<String>();
+			Connection c = DriverManager.getConnection(url, "root", "1234");
+			Statement st = c.createStatement();
+			String sSQL = "SELECT * FROM Paquete";
+			ResultSet rs = st.executeQuery(sSQL);
+			while (rs.next()) {
+				datos1[0] = rs.getString("idPaquete");
+				datos1[1] = rs.getString("nombrePaquete");
+				datos1[2] = rs.getString("pesoPaquete");
+				String data = rs.getString("idPaquete") + " " + rs.getString("nombrePaquete") + " "
+						+ rs.getString("pesoPaquete");
+				datosFileArrayList.add(data);
+				tabla1.addRow(datos1);
+			}
+			table_1.setModel(tabla1);
+			if (imprime == 2) {
+				mtdFileWriter(datosFileArrayList, "paquetes");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
-          table_1.setModel(tabla1);
-      } catch (Exception e) {
-      	System.out.println(e.getMessage());
-      }
-}
+	public void actualizarTablaEnvios(DefaultTableModel tabla2, int imprime) {
+		tabla2 = new DefaultTableModel(null, titulos2);
+		try {
+			ArrayList<String> datosFileArrayList = new ArrayList<String>();
+			Connection c = DriverManager.getConnection(url, "root", "1234");
+			Statement st = c.createStatement();
+			String sSQL = "SELECT * FROM Envio";
+			ResultSet rs = st.executeQuery(sSQL);
 
-public void actualizarTablaEnvios(DefaultTableModel tabla2,  int imprime) {
-	tabla2 = new DefaultTableModel(null, titulos2);
-	try {
-		Connection c = DriverManager.getConnection(url, "root", "1234");
-		Statement st = c.createStatement();
-		String sSQL = "SELECT * FROM Envio";
-		ResultSet rs = st.executeQuery(sSQL);
-
-		while (rs.next()) {
-			datos2[0] = rs.getString("idEnvio");
-			datos2[1] = rs.getString("idPaquete");
-			datos2[2] = rs.getString("idTrabajador");
-			if (imprime==3) {
+			while (rs.next()) {
+				datos2[0] = rs.getString("idEnvio");
+				datos2[1] = rs.getString("idPaquete");
+				datos2[2] = rs.getString("idTrabajador");
 				String data = rs.getString("idEnvio") + " " + rs.getString("idPaquete") + " "
 						+ rs.getString("idTrabajador");
-				System.out.println("DTA "+data);
-				ArrayList<String> datosFileArrayList = new ArrayList<String>();
 				datosFileArrayList.add(data);
+				tabla2.addRow(datos2);
+			}
+			if (imprime == 3) {
 				mtdFileWriter(datosFileArrayList, "envios");
 			}
-			tabla2.addRow(datos2);
+			table_2.setModel(tabla2);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
-		table_2.setModel(tabla2);
-	} catch (Exception e) {
-		System.out.println(e.getMessage());
 	}
-}
-private void mtdFileWriter(ArrayList<String> datosFileArrayList, String deDondeVienen) {
-	try {
-		if (deDondeVienen.equals("trabajadores")) {
-			File trabajadoresFile = new File("trabajadores.txt");
-			createTheFile(datosFileArrayList, trabajadoresFile);
-		} else if (deDondeVienen.equals("paquetes")) {
-			File paquetesFile = new File("paquetes.txt");
-			createTheFile(datosFileArrayList, paquetesFile);
-			System.out.println("Grande del ArrayList: "+datosFileArrayList.size());
-		} else {
-			File enviosFile = new File("envios.txt");
-			createTheFile(datosFileArrayList ,enviosFile);
-		}
-	} catch (Exception e) {
-	}
-}
 
-private void createTheFile(ArrayList<String> datosFileArrayList, File enviosFile) {
-	int veces=0;
-	try {
-		BufferedWriter myWrite = new BufferedWriter(new FileWriter(enviosFile, true));
-		if (enviosFile.getName() == "trabajadores.txt") {
-			myWrite.write("TETASTETASTETASTETASTETASTETASTETASTETASTETAS\n");
-			System.out.println(enviosFile.getName());
-		} else if (enviosFile.getName() == "paquetes.txt") {
-			myWrite.write("IdPaquete 	NombrePaquete	PesoPaquete\n");
-		} else {
-			myWrite.write("IdEnvio	IdPaquete	IdTrabajador\n");
-			veces+=1;
-			System.out.println("Veces "+veces);
+	private void mtdFileWriter(ArrayList<String> datosFileArrayList, String deDondeVienen) {
+		try {
+			if (deDondeVienen.equals("trabajadores")) {
+				File trabajadoresFile = new File("trabajadores.txt");
+				createTheFile(datosFileArrayList, trabajadoresFile);
+			} else if (deDondeVienen.equals("paquetes")) {
+				File paquetesFile = new File("paquetes.txt");
+				createTheFile(datosFileArrayList, paquetesFile);
+				System.out.println("Grande del ArrayList: " + datosFileArrayList.size());
+			} else {
+				File enviosFile = new File("envios.txt");
+				createTheFile(datosFileArrayList, enviosFile);
+			}
+		} catch (Exception e) {
 		}
-		for (String string : datosFileArrayList) {
-			myWrite.write(string+"\n");
-		}
-		myWrite.close();
-	} catch (IOException e) {
-		e.printStackTrace();
 	}
-}
+
+	private void createTheFile(ArrayList<String> datosFileArrayList, File enviosFile) {
+		try {
+			BufferedWriter myWrite = new BufferedWriter(new FileWriter(enviosFile, true));
+			if (enviosFile.getName() == "trabajadores.txt") {
+				myWrite.write("IdTrabajador		Nombre		Apellido	Cargo	Contraseña\n");
+			} else if (enviosFile.getName() == "paquetes.txt") {
+				myWrite.write("IdPaquete 	NombrePaquete	PesoPaquete\n");
+			} else {
+				myWrite.write("IdEnvio	IdPaquete	IdTrabajador\n");
+			}
+			for (String string : datosFileArrayList) {
+				myWrite.write(string + "\n");
+			}
+			myWrite.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
